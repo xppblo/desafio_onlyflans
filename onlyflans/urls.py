14bfd9331d624858b652195e52d701d3dabca961
Pablo Hernández <ppblo.hernandez@gmail.com>
2024-05-28 10:20:42 -0400

    avance proyecto web onlyflans

--- onlyflans/urls.py
+++ onlyflans/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from onlyflansapp.views import plantillaHija1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('plantillaHija1', plantillaHija1)
 ]
--- onlyflansapp/views.py
+++ onlyflansapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def plantillaHija1(request):
+    return render(request, "plantillaHija1.html", {})
